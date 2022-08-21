8546f0a7d1095ddd6223d09ee65faf2b28558916
madtyn <madtyn@gmail.com>
2022-08-22 00:33:58 +0200

    Refactores the whole code. Sorts, makes functions, etc. Small fix for detecting unknown environments in the final deploy_branch_to() function

--- deploy_tools/deploy.py
+++ deploy_tools/deploy.py
@@ -7,6 +7,7 @@ RC_INPUT_MSG = 'RC version? (0 => non-release-candidate, ' \
 
 ROOT_PROJECT = os.path.dirname(os.path.dirname(__file__))
 version_file_path = os.path.join(ROOT_PROJECT, 'config', 'version.py')
+# <MAJ>.<MIN>.<PATCH>-RC<RCV>
 [MAJ, MIN, PATCH, RCV] = range(1, 5)
 PATTERN = r'(\d+)\.(\d+)\.(\d+)(-rc(?P<rcv>\d+))?'
 VERS_FILE = os.path.join(os.getcwd(), '..', 'config', 'version.py')
@@ -16,6 +17,63 @@ RELEASE_VER = '-rc{}'
 CAND_VER = BASIC_VER + RELEASE_VER
 
 TARGET_BRANCH_NAME = 'main'
+
+
+def main():
+    env = ''
+    while env not in ('dev', 'staging', 'production'):
+        env = input('Deploy environment (production, staging and dev for origin): ')
+
+    if env == 'dev':
+        push_all_branches()
+
+    previous_version = get_current_file_version()
+    print(f'Current version is: {previous_version}')
+
+    new_version_input = ask_new_version(previous_version, env)
+
+    new_version_numbers = calculate_new_version_numbers(previous_version, new_version_input, env)
+    new_version = build_version(new_version_numbers)
+    print(f'{previous_version} ==> {new_version}')
+
+    if env != 'dev':
+        check_if_tag_exists(new_version)
+
+    current_branch = os.popen('git symbolic-ref --short -q HEAD').read().strip()
+
+    if 'y' not in input(f'\nConfirm pushing branch {current_branch.upper()} to {TARGET_BRANCH_NAME} for {env.upper()}? [y/n]: '):
+        print('Aborted')
+        sys.exit(0)
+
+    set_file_version(new_version)
+    os.system(f'git commit {version_file_path} -m "({new_version})" ')
+    print('Commited version')
+
+    deploy_branch_to(current_branch, env, new_version)
+
+
+def push_all_branches():
+    """
+    Uploads all branches and exits, without using any version
+    """
+    os.popen(f'git push origin --all')
+    print('Deployment finished')
+    sys.exit(0)
+
+
+def get_current_file_version():
+    """
+    Opens the version.py file and extracts the current version as a string
+    :return: the current version string
+    """
+    global VERS_FILE
+    with open(VERS_FILE, 'r') as ver_file:
+        lines = ver_file.readlines()  # We read the whole file
+        for line in lines:
+            if 'VERSION =' in line:
+                return build_version(get_version_numbers(line))
+    raise EOFError()
+
 
 def get_version_numbers(line):
     """
@@ -34,6 +92,76 @@ def get_version_numbers(line):
     return result
 
 
+def ask_new_version(previous_version, env):
+    """
+    Asks for the new version or set a default one depending on the prveious one
+    :param previous_version: the previous vesion as a string
+    :param env: the environment to deploy to
+    :return: the new version for the deployment as a string
+    """
+    input_new_version = ''
+    while not re.match(PATTERN, input_new_version):
+        current_version = re.search(r'\d+\.\d+\.\d+', previous_version)
+        if current_version:
+            current_version = current_version.group()
+        input_new_version = input(f'New version (Enter for current version {current_version}): ')
+        if input_new_version.strip() == '':
+            input_new_version = current_version
+            if env == 'production':
+                input_new_version = re.sub(r'-rc\d+', r'', input_new_version)
+            break
+    return input_new_version
+
+
+def calculate_new_version_numbers(previous_version, new_version_input, env):
+    """
+    Sets the proper new version numbers for a non-production deployment from the
+    release candidate version input by the user
+    :param previous_version: the previous version as a list of ints
+    :param new_version_input: the new version as a list of ints
+    :param env: the environment to deploy to
+    :return: the new version as a list of ints with the proper release candidate version
+    """
+    # Asks the user if this is a RC and which version
+    input_rel_cand_version = ask_candidate_version(env)
+
+    prev_version_numbers = get_version_numbers(previous_version)
+    new_version_numbers = get_version_numbers(new_version_input)
+
+    if env != 'production':
+        if input_rel_cand_version == '0':
+            pass
+        elif re.match(r'\d+', input_rel_cand_version):
+            new_version_numbers.append(input_rel_cand_version)
+        elif input_rel_cand_version == '':
+            input_rel_cand_version = '1'
+            if len(prev_version_numbers) == 4:
+                if prev_version_numbers[:3] == new_version_numbers:
+                    input_rel_cand_version = str(int(prev_version_numbers[-1]) + 1)
+
+            if len(new_version_numbers) == 3:
+                new_version_numbers.append(input_rel_cand_version)
+            elif len(new_version_numbers) == 4:
+                # Doesn't append but substitutes
+                new_version_numbers[-1] = input_rel_cand_version
+    return new_version_numbers
+
+
+def ask_candidate_version(env):
+    """
+    Asks the user for a candidate version, if any
+    :param env: the environment to deploy to
+    :return: returns the accepted candidate version from the user
+    """
+    input_rel_cand_version = ''
+    if env != 'production':
+        while True:
+            input_rel_cand_version = input(RC_INPUT_MSG).strip()
+            if re.match(r'\d*', input_rel_cand_version):
+                break
+    return input_rel_cand_version
+
+
 def build_version(version_numbers):
     """
     Builds the version string
@@ -47,29 +175,16 @@ def build_version(version_numbers):
     return result.format(*version_numbers)
 
 
-def replace_version(replacement, line):
+def check_if_tag_exists(new_version):
     """
-    Replaces current version with input version if the line matches the pattern
-    :param replacement: the version string
-    :param line: the line in which the replacement is made
-    :return:
+    Check if the version about to be commited already exists as a tag
+    :param new_version: the new version as a string
     """
-    line = re.sub(PATTERN, replacement, line)
-    return line
-
-
-def get_current_file_version():
-    """
-    Opens the version.py file and extracts the current version as a string
-    :return: the current version string
-    """
-    global VERS_FILE
-    with open(VERS_FILE, 'r') as ver_file:
-        lines = ver_file.readlines()  # We read the whole file
-        for line in lines:
-            if 'VERSION =' in line:
-                return build_version(get_version_numbers(line))
-    raise EOFError()
+    # Check if version already exists, abort if positive
+    tags = os.popen('git tag').read()
+    if new_version in tags.split('\n'):
+        print('This version already exists')
+        sys.exit(0)
 
 
 def set_file_version(version):
@@ -97,89 +212,37 @@ def set_file_version(version):
         print('Error processing file')
 
 
-def main():
-    env = input_new_version = ''
-    input_rel_cand_version = ''
-    previous_version = get_current_file_version()
-    print(f'Current version is: {previous_version}')
+def replace_version(replacement, line):
+    """
+    Replaces current version with input version if the line matches the pattern
+    :param replacement: the version string
+    :param line: the line in which the replacement is made
+    :return:
+    """
+    line = re.sub(PATTERN, replacement, line)
+    return line
 
-    while env not in ('dev', 'staging', 'production'):
-        env = input('Deploy environment (production, staging and dev for origin): ')
 
-    if env == 'dev':
-        # No version, just upload all branches and exits
-        os.popen(f'git push origin --all')
-        print('Deployment finished')
-        sys.exit(0)
-
-    while not re.match(PATTERN, input_new_version):
-        current_version = re.search(r'\d+\.\d+\.\d+', previous_version)
-        if current_version:
-            current_version = current_version.group()
-        input_new_version = input(f'New version (Enter for current version {current_version}): ')
-        if input_new_version.strip() == '':
-            input_new_version = current_version
-            if env == 'production':
-                input_new_version = re.sub(r'-rc\d+', r'', input_new_version)
-            break
-
-    if env != 'production':
-        while True:
-            input_rel_cand_version = input(RC_INPUT_MSG).strip()
-            if re.match(r'\d*', input_rel_cand_version):
-                break
-
-    # Completes 'new_version_numbers' and then builds 'new_version' string for writing the file
-    new_version_numbers = get_version_numbers(input_new_version)
-    prev_version_numbers = get_version_numbers(previous_version)
-
-    if env != 'production':
-        if input_rel_cand_version == '0':
-            pass
-        elif re.match(r'\d+', input_rel_cand_version):
-            new_version_numbers.append(input_rel_cand_version)
-        elif input_rel_cand_version == '':
-            input_rel_cand_version = '1'
-            if len(prev_version_numbers) == 4:
-                if prev_version_numbers[:3] == new_version_numbers:
-                    input_rel_cand_version = str(int(prev_version_numbers[-1]) + 1)
-
-            if len(new_version_numbers) == 3:
-                new_version_numbers.append(input_rel_cand_version)
-            elif len(new_version_numbers) == 4:
-                # Doesn't append but substitutes
-                new_version_numbers[-1] = input_rel_cand_version
-
-    new_version = build_version(new_version_numbers)
-
-    if env != 'dev':
-        # Check if version already exists, abort if positive
-        tags = os.popen('git tag').read()
-        if new_version in tags.split('\n'):
-            print('This version already exists')
-            sys.exit(0)
-
-    print(f'{previous_version} ==> {new_version}')
-
-    current_branch = os.popen('git symbolic-ref --short -q HEAD').read().strip()
-
-    if 'y' not in input(f'\nConfirm pushing branch {current_branch.upper()} to {TARGET_BRANCH_NAME} for {env.upper()}? [y/n]: '):
-        print('Aborted')
-        sys.exit(0)
-
-    set_file_version(new_version)
-    os.system(f'git commit {version_file_path} -m "({new_version})" ')
-    print('Commited version')
+def deploy_branch_to(current_branch, env, new_version):
+    """
+    Deploys current branch by pushing it to the selected environment,
+     which is a remote repository
+    :param current_branch: the checked out all-commited branch
+    :param env: the environment to deploy to (remote repo)
+    :param new_version: the new version as a string
+    """
     if env == 'staging':
         command = os.popen(f'git push -f {env} {current_branch}:{TARGET_BRANCH_NAME}')
         command.close()
-        print('Deployment finished.')
     elif env == 'production':
         command = os.popen(f'git push {env} {current_branch}:{TARGET_BRANCH_NAME}')
         error = command.close()
         if not error:
             os.popen(f'git tag {new_version}')
-        print('Deployment finished.')
+    else:
+        print(f'Unknown evironment: {env}')
+        sys.exit(-1)
+    print('Deployment finished.')
 
 
 if __name__ == '__main__':
